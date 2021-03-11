978b8a644629b73b826b30a11296fce273ea7745
enjoys <deadly.info@gmail.com>
2021-03-11 11:24:11 +0300

    wysiwyg refactor

--- src/Summernote.php
+++ src/Summernote.php
@@ -1,12 +1,12 @@
 <?php
-
+declare(strict_types=1);
 
 namespace EnjoysCMS\WYSIWYG\Summernote;
 
-
 use App\Components\Helpers\Assets;
+use App\Components\WYSIWYG\WysiwygInterface;
 
-class Summernote
+class Summernote implements WysiwygInterface
 {
     private string $template;
 
@@ -25,14 +25,14 @@ class Summernote
     {
         Assets::css(
             [
-                __DIR__ . '/node_modules/summernote/dist/summernote-bs4.min.css'
+                __DIR__ . '/../node_modules/summernote/dist/summernote-bs4.min.css'
             ]
         );
 
         Assets::js(
             [
-                __DIR__ . '/node_modules/summernote/dist/summernote-bs4.min.js',
-                __DIR__ . '/node_modules/summernote/lang/summernote-ru-RU.js'
+                __DIR__ . '/../node_modules/summernote/dist/summernote-bs4.min.js',
+                __DIR__ . '/../node_modules/summernote/lang/summernote-ru-RU.js'
             ]
         );
     }
